21a2e4a463969402ed10520c9bab70a20e0c088f
mathiiiiiiiiiiiiiia <162451827+mathiiiiiiiiiiiiiia@users.noreply.github.com>
2024-04-30 11:27:01 -0400

    Add files via upload

---
+++ CV/javascrpit.js
@@ -0,0 +1,6 @@
+var eye= document.getElementById("eye");
+var eye= document.getElementById("Input");
+eye.addEventListener("click", function(){
+    if(input.type == "Password")
+       Input.type = "text"
+       eye.style.opacity=0.8
